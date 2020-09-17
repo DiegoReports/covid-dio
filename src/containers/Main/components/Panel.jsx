b17958185abf8b9b65cbdd94723af8da1259c4c0
unknown <diego.reports@gmail.com>
2020-09-17 19:52:48 -0300

    Update Align

--- src/containers/Main/components/Panel.jsx
+++ src/containers/Main/components/Panel.jsx
@@ -53,8 +53,8 @@ function Panel({ updateAt, onChange, data, country, getCoviddata }) {
     <Card>
       <CardPanelContentStyled>
         <div>
-          <Typography variant="h5" component="span" color="primary">COVID19 </Typography>
-          <Typography variant="h6" component="span" color="primary"> Painel Coronavírus </Typography>
+          <div><Typography variant="h5" component="span" color="primary">COVID19 </Typography></div>
+          <div><Typography variant="h6" component="span" color="primary"> Painel Coronavírus </Typography></div>
           <Typography variant="body2" component="span" color="primary"> Atualizado em: {updateAt}</Typography>
           <div className="pt-2">
             <Select onChange={onChange} value={country}>
