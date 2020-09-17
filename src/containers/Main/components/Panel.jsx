293f41db738d0e73bb27506ed7dff75c6878aa51
unknown <diego.reports@gmail.com>
2020-09-17 17:17:10 -0300

    Update one

--- src/containers/Main/components/Panel.jsx
+++ src/containers/Main/components/Panel.jsx
@@ -8,64 +8,64 @@ import { CardPanelContentStyled, ItemStyled } from './style'
 const navigatorHasShare = navigator.share
 
 function Panel({ updateAt, onChange, data, country, getCoviddata }) {
-    const { cases, recovered, deaths, todayCases, todayDeaths } = data
+  const { cases, recovered, deaths, todayCases, todayDeaths } = data
 
-    const renderCountries = (country, index) => (
-        <MenuItem key={`country-${index}`} value={country.value}>
-            <ItemStyled>
-                <div>{country.label}</div>
-                <img src={country.flag} alt={`País-${country.label}`} />
-            </ItemStyled>
-        </MenuItem>
-    )
-        //Comportamento do Botão Copiar (info)
-    const textCovid19 = `País: ${country} - recuperados: ${recovered}`
+  const renderCountries = (country, index) => (
+    <MenuItem key={`country-${index}`} value={country.value}>
+      <ItemStyled>
+        <div>{country.label}</div>
+        <img src={country.flag} alt={`País-${country.label}`} />
+      </ItemStyled>
+    </MenuItem>
+  )
 
-    const copyInfo = () => {
-        navigator.clipboard.writeText(textCovid19)
-    }
+  const textCovid19 = `País: ${country} - recuperados: ${recovered}`
 
-    const shareInfo = () => {
-        navigator.share({
-            title: `Dados da Covid 19 - ${country}`,
-            text: textCovid19,
-            url:'https://covid-dio.github.io'
-        })
-    }
+  const copyInfo = () => {
+    navigator.clipboard.writeText(textCovid19)
+  }
 
-    const renderShareButton = (
+  const shareInfo = () => {
+    navigator.share({
+      title: `Dados do Covid19 - ${country}`,
+      text: textCovid19,
+      url: 'https://covid19dio.netlify.app/'
+    })
+  }
+
+  const renderShareButton = (
+    <div>
+      <Button variant="contained" color="primary" onClick={shareInfo}>
+        Compartilhar
+      </Button>
+    </div>
+  )
+
+  const renderCopyButton = (
+    <div>
+      <Button variant="contained" color="primary" onClick={copyInfo}>
+        Copiar
+      </Button>
+    </div>
+  )
+
+  return (
+    <Card>
+      <CardPanelContentStyled>
         <div>
-            <Button variant="contained" color="primary" onClick={shareInfo}>
-                Compartilhar
-            </Button>
+          <Typography variant="h5" component="span" color="primary">COVID19</Typography>
+          <Typography variant="h6" component="span" color="primary">Painel Coronavírus</Typography>
+          <Typography variant="body2" component="span" color="primary">Atualizado em: {updateAt}</Typography>
+          <div className="pt-2">
+            <Select onChange={onChange} value={country}>
+              {COUNTRIES.map(renderCountries)}
+            </Select>
+          </div>
         </div>
-    )
-
-    const renderCopyButton = (
-        <div>
-            <Button variant="contained" color="primary" onClick={copyInfo}>
-                Copiar
-            </Button>
-        </div>
-    )
-
-    return (
-        <Card>
-            <CardPanelContentStyled>
-                <div>
-                    <Typography variant="h5" component="span" color="primary">COVID19</Typography>
-                    <Typography variant="h6" component="span" color="primary">Painel Coronavirus</Typography>
-                    <Typography variant="body2" component="span" color="primary">Atualizado em: {updateAt}</Typography>
-                    <div className="pt-2">
-                        <Select onChange={onChange} value={country}>
-                            {COUNTRIES.map(renderCountries)}
-                        </Select>
-                    </div>
-                </div>
-                {navigatorHasShare ? renderShareButton : renderCopyButton}
-            </CardPanelContentStyled>
-        </Card>
-    )
+        {navigatorHasShare ? renderShareButton : renderCopyButton}
+      </CardPanelContentStyled>
+    </Card>
+  )
 }
 
-export default memo(Panel) 
+export default memo(Panel)
